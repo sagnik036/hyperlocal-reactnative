adc3aa416f8d93e3cf675993965cfeac9c855c84
Kalpita Chaudhuri <kalpitachaudhuri01@gmail.com>
2023-05-08 17:19:56 +0530

    Delivery person profile screen added

--- src/Postlogin/Delivery Person Screens/Dprofile.js
+++ src/Postlogin/Delivery Person Screens/Dprofile.js
@@ -1,6 +1,8 @@
-import { View, Text, StyleSheet, Dimensions } from 'react-native'
+import { View, Text, StyleSheet, Dimensions, Image, } from 'react-native'
 import React from 'react'
+import { TouchableOpacity } from 'react-native-gesture-handler';
 import { IconButton } from 'react-native-paper'
+import { color } from 'react-native-reanimated';
 
 const { height, width } = Dimensions.get("window");
 
@@ -15,7 +17,59 @@ export default function Dprofile({navigation}) {
       size={30}
       style={styles.menu}
     />
-    <Text>Profile</Text>
+    <Text style={styles.Dtext}>Hey</Text>
+    <Text style={styles.Dname}>
+        Kalpita
+    </Text>
+    
+    <Image
+        style={styles.Dprofilepic}
+        source={{
+          uri: "https://www.forbesindia.com/media/wpower2020/Monika%20Shergill.jpg",
+        }}
+      />
+      <IconButton
+        icon="delete"
+        iconColor="black"
+        mode="contained"
+        size={21}
+        style={styles.delete}
+      />
+      <Text style={styles.Dlog}>Logged in via 7439161715?_________</Text>
+      
+      <View style={styles.roundedrect}>
+        <Text style={styles.Dstatus}>Delivery Status</Text>
+        <Text style={styles.Dstatusn}>Total 
+        <Text style={{fontWeight: '500'}}> 4 </Text>
+         deliveries till now.</Text>
+      </View>
+      
+      <View style={styles.roundedrect2}>
+      <Text style={styles.Vdetails}>Vehicle Details</Text>
+        <Text style={styles.Vmodel}>Tata 407 Gold SFC Truck</Text>
+        <Text style={styles.wheel}>4 wheeler</Text>
+        <Text style={styles.Vnumber}>Vehicle Number
+        <Text style={{fontWeight: '400'}}>    HR12DE1433</Text>
+        </Text>
+      </View>
+      
+      <View style={{ padding: 40 }}>
+        <TouchableOpacity
+          style={{
+            justifyContent: "center",
+            padding: 10,
+            borderRadius: 7,
+            backgroundColor: "#F02121",
+            height: 60,
+            bottom: -55,
+            //left: -70,
+            }}>
+          <Text style={{textAlign: "center", color: "white", fontSize: 20, fontWeight: "500",}}>
+            Delete Account
+          </Text>
+        </TouchableOpacity>
+      </View>
+
   </View>
   )
 }
@@ -33,5 +87,113 @@ const styles = StyleSheet.create({
       backgroundColor: "#fff",
       alignItems: "center",
       justifyContent: "center",
+    },
+    Dtext: {
+      position: "absolute",
+      alignItems: "center",
+      display: "flex",
+      width: 182,
+      height: 25,
+      left: 50,
+      top: 100,
+      fontSize: 18,
+      color: "#5C5C5C",
+      fontWeight: "500",
+    },
+    Dname: {
+      position: "absolute",
+      alignItems: "center",
+      display: "flex",
+      width: 117,
+      height: 100,
+      left: 50,
+      top: 125,
+      fontSize: 30,
+      color: "#0F0F0F",
+      fontWeight: "800",
+    },
+    Dprofilepic: {
+      position: "absolute",
+      width: 80,
+      height: 80,
+      left: 270,
+      top: 105,
+      borderRadius: 80,
+    },
+    delete: {
+      position: "absolute",
+      right: 30,
+      top: 80,
+      bottom: 88,
+      backgroundColor: "white",
+      justifyContent: "center",
+      borderRadius: 50,
+      alignItems: "center",
+    },
+    Dlog: {
+      position: "absolute",
+      alignItems: "center",
+      display: "flex",
+      width: 500,
+      height: 25,
+      left: 50,
+      top: 170,
+      fontSize: 13,
+      color: "#5C5C5C",
+      fontWeight: "400",
+    },
+    roundedrect: {
+      alignItems: 'flex-start',
+      justifyContent: 'flex-start',
+      bottom: -20,
+      width: 350,
+      height: 90,
+      borderRadius: 20,
+      borderWidth: 0.5,
+    },
+    Dstatus: {
+      left: 25,
+      top: 15,
+      fontSize: 16,
+      fontWeight: '500',
+    },
+    Dstatusn: {
+      left: 25,
+      top: 20,
+      fontSize: 22,
+      fontWeight: '300',
+    },
+    roundedrect2: {
+      alignItems: 'flex-start',
+      justifyContent: 'flex-start',
+      bottom: -55,
+      width: 350,
+      height: 190,
+      borderRadius: 20,
+      borderWidth: 0.5,
+    },
+    Vdetails: {
+      left: 25,
+      top: 15,
+      fontSize: 16,
+      fontWeight: '500',
+    },
+    Vmodel: {
+      left: 25,
+      top: 25,
+      fontSize: 25,
+      fontWeight: '800',
+    },
+    wheel: {
+      left: 25,
+      top: 30,
+      fontSize: 16,
+      fontWeight: '400',
+    },
+    Vnumber: {
+      left: 25,
+      top: 60,
+      fontSize: 16,
+      fontWeight: '500',
     },
 })
