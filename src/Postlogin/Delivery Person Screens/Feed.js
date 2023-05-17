7ce73c568ca6cec03e88d1bbc13366233ba40ca5
Sayantika Dey <Sayantikasuri@gmail.com>
2023-05-17 13:32:19 +0530

    small changes

--- src/Postlogin/Delivery Person Screens/Feed.js
+++ src/Postlogin/Delivery Person Screens/Feed.js
@@ -1,10 +1,29 @@
 import { View, Text, StyleSheet, Dimensions } from 'react-native'
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import { IconButton } from 'react-native-paper'
-
+import { API_URl } from "@env";
+import axios from 'axios';
+import { Authcontext } from '../../../api/Authcontext';
 const { height, width } = Dimensions.get("window");
 
 export default function Feed({navigation}) {
+
+  const { userInfo } = useContext(Authcontext);
+  const token = userInfo.token.access 
+  const feedapicall = () => {
+    axios.get(`${API_URl}/live-jobs/`,{headers:{
+      Authorization:`Bearer ${token}`
+    }}) .then((res) =>{
+      console.log (res.data)
+    }) .catch((err) =>{
+      console.log (err)
+    })
+  } 
+
+  useEffect (() => {
+    feedapicall()
+  }, [])
+
   return (
     <View style={styles.container}>
       <IconButton onPress={()=>navigation.openDrawer()}
