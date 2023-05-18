bf5c4dbff5f68c47865df50c33c32fcbe2b48bcd
Sayantika Dey <Sayantikasuri@gmail.com>
2023-05-18 11:44:02 +0530

    flatlist some contents added

--- src/Postlogin/Delivery Person Screens/Feed.js
+++ src/Postlogin/Delivery Person Screens/Feed.js
@@ -1,20 +1,24 @@
 import { View, Text, StyleSheet, Dimensions } from 'react-native'
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { IconButton } from 'react-native-paper'
 import { API_URl } from "@env";
 import axios from 'axios';
 import { Authcontext } from '../../../api/Authcontext';
+import { FlatList } from 'react-native-gesture-handler';
 const { height, width } = Dimensions.get("window");
 
 export default function Feed({navigation}) {
 
   const { userInfo } = useContext(Authcontext);
+  const [data, setData] = useState([]);
+
   const token = userInfo.token.access 
   const feedapicall = () => {
     axios.get(`${API_URl}/live-jobs/`,{headers:{
       Authorization:`Bearer ${token}`
     }}) .then((res) =>{
-      console.log (res.data)
+      console.log (res.data.results[0].job_id)
+      setData ([res.data.results[0]])
     }) .catch((err) =>{
       console.log (err)
     })
@@ -23,6 +27,8 @@ export default function Feed({navigation}) {
   useEffect (() => {
     feedapicall()
   }, [])
+
+
 
   return (
     <View style={styles.container}>
@@ -33,7 +39,18 @@ export default function Feed({navigation}) {
         size={30}
         style={styles.menu}
       />
-      <Text>Feed</Text>
+      <View style={{width:"100%", marginTop:50}}>
+      <FlatList 
+      data={data}
+      keyExtractor={(item)=>item.job_id}
+      renderItem={({item,index})=>
+      <View>
+        <Text>{item.id}</Text>
+        <Text>{item.estimated_duration}</Text>
+        <Text>{item.pickup_contact_name}</Text>
+      </View>}
+      />
+      </View>
     </View>
   )
 }
